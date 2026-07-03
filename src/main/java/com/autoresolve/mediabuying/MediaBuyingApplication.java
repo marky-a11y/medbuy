ed5376edd0f54717ad519bfd341d40ba8f17f8a4
marky-a11y <markyake@gmail.com>
2026-07-03 09:16:24 -0400

    Update MediaBuyingApplication.java
    adding more logging to see if I can see a cause for the failed server start

--- src/main/java/com/autoresolve/mediabuying/MediaBuyingApplication.java
+++ src/main/java/com/autoresolve/mediabuying/MediaBuyingApplication.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -29,6 +33,23 @@ public class MediaBuyingApplication {
                     env.getProperty("server.port"),
                     java.util.Arrays.toString(env.getActiveProfiles()),
                     System.getenv("PORT"));
+        };
+    }
+
+    @Bean
+    ApplicationListener<ApplicationStartedEvent> started() {
+        return event -> log.info("=== ApplicationStartedEvent ===");
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> ready() {
+        return event -> log.info("=== ApplicationReadyEvent ===");
+    }
+
+    @Bean
+    ApplicationListener<ApplicationFailedEvent> failed() {
+        return event -> {
+            log.error("=== ApplicationFailedEvent ===", event.getException());
         };
     }
 }
